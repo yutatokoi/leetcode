297b3858ada997aa3a34cad3f22d2c2c28625302
yutatokoi <yutatokoi@proton.me>
2024-03-10 07:38:08 +1100

    feat: Q1 attempt3

---
+++ 0001-two-sum/attempt3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = dict()
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in nums_dict:
+                return [nums_dict[diff], i]
+
+            nums_dict[n] = i
