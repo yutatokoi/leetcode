8cf88b2f5f6fea5bdf0f15427d1249de43292ff1
yutatokoi <yutatokoi@proton.me>
2024-03-15 09:30:09 +1100

    feat: Q217 attempt4

---
+++ 0217-contains-duplicate/attempt4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums_set = set()
+
+        for n in nums:
+            if n in nums_set:
+                return True
+            else:
+                nums_set.add(n)
+        
+        return False
+        
