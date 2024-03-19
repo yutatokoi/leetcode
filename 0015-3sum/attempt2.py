8c8df405b20436e6d57e6ea14b53871036a2c992
yutatokoi <yutatokoi@proton.me>
2024-03-19 14:20:56 +1100

    feat: Q15 attempt2

---
+++ 0015-3sum/attempt2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i - 1]:
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                three_sum = nums[i] + nums[l] + nums[r]
+                if three_sum > 0:
+                    r -= 1
+                elif three_sum < 0:
+                    l += 1
+                elif three_sum == 0:
+                    result.append([n, nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+            
+        return result
+            
