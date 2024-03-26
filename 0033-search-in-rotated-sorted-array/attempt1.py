9358d0668bf0cce9ae3cafb34873884e80a73d64
yutatokoi <yutatokoi@proton.me>
2024-03-26 21:04:53 +1100

    feat: Q33 attempt1

---
+++ 0033-search-in-rotated-sorted-array/attempt1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            
+            if nums[m] >= nums[l]:
+                if nums[m] < target or nums[l] > target:
+                    l = m + 1
+                else:
+                    r = m - 1
+            elif nums[m] < nums[r]:
+                if nums[m] > target or nums[r] < target:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return -1
+
