faa45c315da2bcda79f23127decea6f66077d88a
yutatokoi <yutatokoi@proton.me>
2024-03-31 19:58:16 +1100

    feat: Q567 attempt2

---
+++ 0567-permutation-in-string/attempt2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            
+            s2_count[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+        for i in range(26):
+            if s1_count[i] == s2_count[i]:
+                matches += 1
+            else:
+                matches += 0
+        
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            s2_count[index] += 1
+            if s1_count[index] == s2_count[index]:
+                matches += 1
+            elif s1_count[index] + 1 == s2_count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2_count[index] -= 1
+            if s1_count[index] == s2_count[index]:
+                matches += 1
+            elif s1_count[index] - 1 == s2_count[index]:
+                matches -= 1
+
+            l += 1
+
+        return matches == 26            
+
