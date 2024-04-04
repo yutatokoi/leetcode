86cdbb71f7c31f3027200a118f1181baed610260
yutatokoi <yutatokoi@proton.me>
2024-04-05 08:49:11 +1100

    feat: Q138 attempt1

---
+++ 0138-copy-list-with-random-pointer/attempt1.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        old_to_copy = {None : None}
+
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            old_to_copy[curr] = copy
+            curr = curr.next
+
+        curr = head
+        while curr:
+            copy = old_to_copy[curr]
+            copy.next = old_to_copy[curr.next]
+            copy.random = old_to_copy[curr.random]
+            curr = curr.next
+
+        return old_to_copy[head] 
