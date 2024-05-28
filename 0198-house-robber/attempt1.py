ac9b0b52c0736076054e02e7fa0f8f75ec72a6d3
yutatokoi <yutatokoi@proton.me>
2024-05-29 08:45:55 +1000

    feat: Q198 attempt1

---
+++ 0198-house-robber/attempt1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+
+        # [rob1, rob2, n, n+1, n+2, ...]
+        for n in nums:
+            temp = max(rob1 + n, rob2)
+
+            rob1 = rob2
+            rob2 = temp
+
+        return rob2
+
+        
