a9d87ad769f808a3353f0024ae60fe92960ab7bb
yutatokoi <yutatokoi@proton.me>
2024-06-09 07:17:46 +1000

    feat: Q56 attempt1

---
+++ 0056-merge-intervals/attempt1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda i : i[0])
+        output = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            last_end = output[-1][1]
+
+            if start <= last_end:
+                output[-1][1] = max(last_end, end)
+            else:
+                output.append([start, end])
+
+        return output
+
+            
