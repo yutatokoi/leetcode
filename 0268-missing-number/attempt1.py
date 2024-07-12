2218c0db08f3237eadc5007b58c65ea55de9a949
yutatokoi <yutatokoi@proton.me>
2024-07-13 07:26:01 +0900

    feat: Q268 attempt1

---
+++ 0268-missing-number/attempt1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += (i - nums[i])
+
+        return res
+
+        
