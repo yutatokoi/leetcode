b5c8a3ba18f6813c5781a41b8b1ed454750152bd
yutatokoi <yutatokoi@proton.me>
2024-07-23 09:54:27 +1000

    feat: Q22 attemp3

---
+++ 0022-generate-parentheses/attemp3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+
+        def backtrack(opened, closed):
+            if n == opened == closed:
+                res.append("".join(stack))
+                return
+            if opened < n:
+                stack.append("(")
+                backtrack(opened + 1, closed)
+                stack.pop()
+            if closed < opened:
+                stack.append(")")
+                backtrack(opened, closed + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
+
+        
