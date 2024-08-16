f8abf0a1731c1b08b5f565226fd8ef06faa92cf9
yutatokoi <yutatokoi@proton.me>
2024-08-16 18:00:55 +1000

    feat: Q88 attempt1

---
+++ 0088-merge-sorted-array/attempt1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if n == 0: return
+
+        len1 = len(nums1)
+        last_index = len1 - 1
+        while n > 0 and m > 0:
+            if nums2[n - 1] >= nums1[m - 1]:
+                nums1[last_index] = nums2[n - 1]
+                n -= 1
+            else:
+                nums1[last_index] = nums1[m - 1]
+                m -= 1
+            
+            last_index -= 1
+
+        while n > 0:
+            nums1[last_index] = nums2[n - 1]
+            n -= 1
+            last_index -= 1
+            
+        
