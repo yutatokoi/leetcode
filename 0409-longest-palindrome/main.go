af05641d87783affadb77872c5ceec67affbf9b3
yutatokoi <yutatokoi@proton.me>
2024-10-18 05:40:22 +1100

    feat: Q409 Go attempt2

--- 0409-longest-palindrome/main.go
+++ 0409-longest-palindrome/main.go
@@ -16,3 +16,31 @@ func longestPalindrome(s string) int {
 
     return pairs * 2 + 1
 }
+
+// attempt2 (array)
+import "unicode"
+
+func longestPalindrome(s string) int {
+    pairs := 0
+    count := make([]int, 52)
+
+    for _, ch := range s {
+        var index int
+        if unicode.IsUpper(ch) {
+            index = int(ch) - int('A') + 26
+        } else {
+            index = int(ch) - int('a')
+        }
+        count[index]++
+
+        if count[index] % 2 == 0 {
+            pairs++
+        }
+    }
+
+    if pairs * 2 == len(s) {
+        return len(s)
+    }
+
+    return pairs * 2 + 1
+}
