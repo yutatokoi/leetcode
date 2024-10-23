cab8b9f4f494d1d2aee03f00287e9b5eb9fcf43a
yutatokoi <yutatokoi@proton.me>
2024-10-23 18:32:11 +1100

    feat: Q434 Go attempt2

--- 0434-number-of-segments-in-a-string/main.go
+++ 0434-number-of-segments-in-a-string/main.go
@@ -1,7 +1,21 @@
+// attempt1 (built-in function)
 import "strings"
 
 func countSegments(s string) int {
     words := strings.Fields(s)
 
     return len(words)    
+}
+
+// attempt2 O(1) extra space, O(n) time
+func countSegments(s string) int {
+    segments := 0
+
+    for i := 0; i < len(s); i++ {
+        if (i == 0 || s[i - 1] == ' ') && (s[i] != ' ') {
+            segments++
+        }
+    }
+
+    return segments
 }
