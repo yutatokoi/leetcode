b6516d90ee46876693762b4312df807cd1fb4a78
yutatokoi <yutatokoi@proton.me>
2024-11-08 18:00:06 +1100

    feat: Q509 Python attempt2

--- 0509-fibonacci-number/main.py
+++ 0509-fibonacci-number/main.py
@@ -1,3 +1,4 @@
+# attempt1: naive recursive solution
 class Solution:
     def fib(self, n: int) -> int:
         if n == 0:
@@ -6,3 +7,13 @@ class Solution:
             return 1
 
         return self.fib(n - 1) + self.fib(n - 2)
+
+# attempt2: DP (?) solution
+class Solution:
+    def fib(self, n: int) -> int:
+        numbers = [0] * 31
+        numbers[1] = 1
+        for i in range(2, n + 1):
+            numbers[i] = numbers[i-1] + numbers[i - 2]
+
+        return numbers[n]
