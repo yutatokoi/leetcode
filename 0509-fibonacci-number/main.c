c4768a1561cbdbc55c920aad550289ce54794366
yutatokoi <yutatokoi@proton.me>
2024-11-08 18:03:41 +1100

    feat: Q509 C attempt2

--- 0509-fibonacci-number/main.c
+++ 0509-fibonacci-number/main.c
@@ -5,3 +5,13 @@ int fib(int n){
 
     return fib(n - 1) + fib(n - 2);
 }
+
+// attempt2: DP (?) solution
+int fib(int n){
+    int numbers[31] = {0};
+    numbers[1] = 1;
+
+    for (int i = 2; i < n + 1; i++) numbers[i] = numbers[i-1] + numbers[i-2];
+
+    return numbers[n];
+}
