e36f3e69a839aa164b681505c10af20980da423b
yutatokoi <yutatokoi@proton.me>
2024-11-10 16:15:43 +1100

    keep latest attempt, rename to main

--- 0215-kth-largest-element-in-an-array/attempt1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        heapq.heapify(nums)
-        while len(nums) + 1 > k:
-            res = heapq.heappop(nums)
-
-        return res
-        
-
--- 0217-contains-duplicate/attempt1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        for i in nums:
-            if nums[i] in nums[i+1::]:
-                return True
-            else:
-                return False
