a3342880194acfbe50fe074266e66f58266dc80e
Yuta Tokoi <yutatokoi@proton.me>
2024-12-01 09:11:30 +0900

    feat: Q653 Python attempt1

---
+++ Python/0653.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        seen = set()
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+            if k - node.val in seen:
+                return True 
+            
+            seen.add(node.val)
+            
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+        return False
