9aaa5c7b41f3ede34b169e89a6847af8dba79b49
Yuta Tokoi <yutatokoi@proton.me>
2024-12-12 07:42:45 +0900

    feat: Q705 Python attempt2

--- Python/0705.py
+++ Python/0705.py
@@ -1,3 +1,4 @@
+# attempt1 (not really a hashset)
 class MyHashSet:
 
     def __init__(self):
@@ -11,6 +12,38 @@ class MyHashSet:
 
     def contains(self, key: int) -> bool:
         return self.hashset[key]
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
+
+# attempt2 (simple hashing algorithm, unsorted arrays as buckets)
+
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+    def add(self, key: int) -> None:
+        index = self._hash(key)
+        if key not in self.buckets[index]:
+            self.buckets[index].append(key)
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        if key in self.buckets[index]:
+            self.buckets[index].remove(key)
+
+    def contains(self, key: int) -> bool:
+        index = self._hash(key)
+        return key in self.buckets[index]
+
+    def _hash(self, key: int) -> int:
+        return key % self.size
 
 
 # Your MyHashSet object will be instantiated and called as such:
