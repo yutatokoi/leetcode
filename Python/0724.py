54537083b7c3f08fce451ff8967967563d2474d1
Yuta Tokoi <yutatokoi@proton.me>
2024-12-16 07:34:30 +0900

    feat: Q724 Python attempt1

---
+++ Python/0724.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        leftsum = 0 
+        for i, n in enumerate(nums):
+            if leftsum == (total - leftsum - n):
+                return i
+            leftsum += n
+
+        return -1
