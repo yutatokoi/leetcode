25bdf8b857c8de51f1824d7fdcae2b901190677c
Yuta Tokoi <yutatokoi@proton.me>
2024-12-20 08:01:27 +0900

    feat: Q734 Python attempt2

--- Python/0734.py
+++ Python/0734.py
@@ -1,3 +1,4 @@
+# attempt1 (naive solution)
 class Solution:
     def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
         if len(sentence1) != len(sentence2):
@@ -10,5 +11,24 @@ class Solution:
                 continue
             else:
                 return False
+
+        return True
+
+# attempt2 (hashmap solution)
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        if len(sentence1) != len(sentence2):
+            return False
+
+        word_to_similar_words = collections.defaultdict(set)
+
+        for word1, word2 in similarPairs:
+            word_to_similar_words[word1].add(word2)
+            word_to_similar_words[word2].add(word1)
+
+        for i in range(len(sentence1)):
+            if sentence1[i] == sentence2[i] or sentence2[i] in word_to_similar_words[sentence1[i]]:
+                continue
+            return False
 
         return True
