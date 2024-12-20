7d4296b8799cc8ee32fc8f9ab79a2ecf3442317b
Yuta Tokoi <yutatokoi@proton.me>
2024-12-21 07:54:25 +0900

    feat: Q744 Python attempt1

---
+++ Python/0744.py
@@ -0,0 +1,7 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for letter in letters:
+            if ord(letter) > ord(target):
+                return letter
+
+        return letters[0]
