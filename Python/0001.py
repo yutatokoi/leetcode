7f50c65e866bfc64d1facd09ff4ee7e5f254f885
Yuta Tokoi <yutatokoi@proton.me>
2025-03-09 17:29:56 +1100

    add O(n) single-pass solution

--- Python/0001.py
+++ Python/0001.py
@@ -1,3 +1,4 @@
+# O(n^2) solution. Simple, but suboptimal
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
@@ -5,4 +6,18 @@ class Solution:
                 if target == (nums[i] + nums[j]):
                     return [i, j]
 
-        
+# O(n) solution with just one-pass through array
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = dict()
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in dictionary:
+                return [dictionary[complement], i]
+                
+            dictionary[nums[i]] = i
+
+        return []
